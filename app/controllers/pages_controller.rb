a5168387f9f0d0bb34fe0330577ed8011ea120c7
Lisa Millet <lisamilletpro@gmail.com>
2023-03-22 22:05:40 +0100

    routes + controller pages + views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,26 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def support
+  end
+
+  def news
+  end
+
+  def contact
+  end
+
+  def directory_artisans
+  end
+
+  def blog
+  end
+
+  def podcast
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get '/qrcode/:slug', to: 'scanner#qr_code', as: 'qr_code'
+  get '/qui_sommes_nous', to: 'pages#about', as: 'about'
+  get '/soutenir', to: 'pages#support', as: 'support'
+  get '/actualites', to: 'pages#news', as: 'news'
+  get '/contact', to: 'pages#contact', as: 'contact'
+  get '/repertoire_artisans', to: 'pages#directory_artisans', as: 'directory_artisans'
+  get '/blog', to: 'pages#blog', as: 'blog'
+  get '/podcast', to: 'pages#podcast', as: 'podcast'
 end
