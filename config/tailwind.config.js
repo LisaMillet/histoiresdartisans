fdaaeca9287182c0ec52826de3b655428a5d5f36
Lisa Millet <lisamilletpro@gmail.com>
2023-04-10 17:22:04 +0200

    tailwind tests 3

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,7 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{html,js,erb}"],
+  purge: ['./app/**/*.html.erb', './app/helpers/**/*.rb', './app/javascript/**/*.js'],
+  darkMode: false, // ou 'media' ou 'class'
   theme: {
+    extend: {
+      padding: {
+        '2': '0.5rem',
+      },
+      margin: {
+        '8': '2rem',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
