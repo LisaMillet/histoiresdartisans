a528abba85b24d70a4eeedb98c79c0d5f055090b
Lisa Millet <lisamilletpro@gmail.com>
2023-04-10 15:56:41 +0200

    ne pas merger, plein de fautes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,36 +1,8 @@
-// const defaultTheme = require('tailwindcss/defaultTheme')
-
-// module.exports = {
-//   content: [
-//     './public/*.html',
-//     './app/helpers/**/*.rb',
-//     './app/javascript/**/*.js',
-//     './app/views/**/*.{erb,haml,html,slim}'
-//   ],
-//   theme: {
-//     extend: {
-//       // screens: {
-//       //   'sm': '640px',
-//       //   // => @media (min-width: 640px) { ... }
-
-//       //   'md': '768px',
-//       //   // => @media (min-width: 768px) { ... }
-
-//       //   'lg': '1024px',
-//       //   // => @media (min-width: 1024px) { ... }
-
-//       //   'xl': '1280px',
-//       //   // => @media (min-width: 1280px) { ... }
-
-//       //   '2xl': '1536px',
-//       //   // => @media (min-width: 1536px) { ... }
-//       // }
-//     },
-//   },
-//   plugins: [
-//     require('@tailwindcss/forms'),
-//     require('@tailwindcss/aspect-ratio'),
-//     require('@tailwindcss/typography'),
-//     require('@tailwindcss/container-queries'),
-//   ]
-// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js,erb}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
