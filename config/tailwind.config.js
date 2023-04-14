04b5b1454391abc302ad25bf304348fe061348d7
Lisa Millet <lisamilletpro@gmail.com>
2023-04-14 15:07:08 +0200

    refresh from start avec Tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,10 +8,20 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    fontFamily: {
+      monospace: ['Be Vietnam Pro', 'sans-serif'],
+      sans: ['Roboto Mono', 'monospace'],
+    },
+
+    colors: {
+      'dark-green': '#002428',
+      'green': '#00594F',
+      'dark-orange': '#963900',
+      'orange': '#B94700',
+      'beige': '#F2E7DD',
+    },
+
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
     },
   },
   plugins: [
