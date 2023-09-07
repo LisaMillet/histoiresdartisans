603b7dfe7fa9f5a277e257b9757effc990f91d39
Alizee Vallat <alizee.v@live.fr>
2023-09-07 12:02:27 +0200

    cut the long titles on cards

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,14 +58,13 @@ class PostsController < ApplicationController
   # end
 
   private
-    # # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :content, :author)
-    end
-
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :content, :author)
+  end
 end
