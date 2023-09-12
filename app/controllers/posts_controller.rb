c008397fea35c982d9685edcedf72dad809956a1
Alizee Vallat <alizee.v@live.fr>
2023-09-12 11:31:45 +0200

    adding the logic admin to User, logic authenticate user before Post: new,create, edit, update, delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_post, only: %i[show edit update destroy]
   skip_before_action :verify_authenticity_token, only: :destroy
 
