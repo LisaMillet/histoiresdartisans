96974c9da18fa7821e4336346cb38f246835918b
Alizee Vallat <alizee.v@live.fr>
2023-09-12 10:59:46 +0200

    adding the gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   resources :contacts, only: %i[new create]
   get 'contacts/sent', to: 'contacts#sent', as: 'sent_contact'
