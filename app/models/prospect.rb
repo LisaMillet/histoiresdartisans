d6ee391c28f2de6e2f3daa7667afb9bb05f5c323
Thibault Chassine <thibault.chassine@gmail.com>
2023-09-15 11:03:47 +0200

    added callback to clean email after create

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -17,4 +17,9 @@ class Prospect < ApplicationRecord
 
   validates :first_name, :last_name, :email, :position, :company, presence: true
   validates :position, inclusion: { in: POSITIONS }
+  after_create :clean_email
+
+  def clean_email
+    update(email: email.strip.downcase)
+  end
 end
