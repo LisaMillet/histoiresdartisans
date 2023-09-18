a0b04f275af194de57bfd3c5d4c1498b1bed4577
Thibault Chassine <thibault.chassine@gmail.com>
2023-09-18 16:19:47 +0200

    trigger form on click

--- app/javascript/controllers/form_prospect_controller.js
+++ app/javascript/controllers/form_prospect_controller.js
@@ -2,6 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form-prospect"
 export default class extends Controller {
+
+  static targets = [
+    'divForm',
+    'downloadButton'
+  ]
+
   connect() {
+    console.log('hellooooooooooo')
+  }
+
+  toggleForm(event) {
+    event.preventDefault()
+    const btn = event.target
+
+    this.divFormTargets.forEach(div => {
+      if (div.dataset.id === btn.dataset.id) {
+        btn.classList.add('hidden')
+        div.classList.remove('hidden')
+      } else {
+        this.downloadButtonTargets.forEach(btnDL => {
+          if (btnDL !== btn) btnDL.classList.remove('hidden')
+        });
+        div.classList.add('hidden')
+      }
+    });
   }
 }
