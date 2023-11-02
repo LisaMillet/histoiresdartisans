b74b1152262bbc6f731ba31f51e2249ff4ffb63b
Alizee Vallat <alizee.v@live.fr>
2023-11-02 19:52:41 +0100

    fix bug post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: "Post was successfully created." }
+        format.html { redirect_to posts_path, notice: "Votre post est publié." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
